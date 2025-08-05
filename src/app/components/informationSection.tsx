0b0f7272c58091e8ef50c967460b531c2d0b0fd5
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-08-05 00:41:55 -0400

    Completed the information section

--- src/app/components/informationSection.tsx
+++ src/app/components/informationSection.tsx
@@ -7,7 +7,8 @@ export default function InformationSection() {
                 Leading the Way in <br /> Sustainable Transportation
             </div>
 
-            <div className="bg-[#262626] mt-20 w-[95%] relative">
+            {/* E-bike light section */}
+            <div className="bg-[#262626] mt-20 w-[95%]">
                 <Image
                     src="/E-bike-light.svg"
                     alt="Ebike front light"
@@ -16,13 +17,54 @@ export default function InformationSection() {
                     height={400}
                     priority
                 />
+            </div>
+            <div className="w-[95%] pt-4">
+                <p className="text-4xl font-semibold pb-4">High-quality materials</p>
+                <p className="text-2xl text-neutral-400">
+                    Premium materials and advanced construction techniques to ensure durability and a long lifespan
+                </p>
+            </div>
 
-                {/* Overlay Text */}
-                <div className="absolute bottom-8 left-8">
-                    <p className="text-4xl font-semibold pb-5">High-quality materials</p>
-                    <p className="text-2xl max-w-xl text-neutral-400">
-                        Premium materials and advanced construction techniques to ensure durability and a long lifespan
-                    </p>
+            {/* Row of 2 image sections side by side */}
+            <div className="flex flex-row mt-20 w-[95%] gap-5">
+                {/* E-bike map */}
+                <div className="w-1/2">
+                    <div className="bg-[#262626]">
+                        <Image
+                            src="/E-bike-map.svg"
+                            alt="E bike map view"
+                            layout="responsive"
+                            width={1600}
+                            height={400}
+                            priority
+                        />
+                    </div>
+                    <div className="pt-4">
+                        <p className="text-4xl font-semibold pb-4">High-quality materials</p>
+                        <p className="text-2xl text-neutral-400">
+                            Premium materials and advanced construction techniques to ensure durability and a long lifespan.
+                        </p>
+                    </div>
+                </div>
+
+                {/* Bike together */}
+                <div className="w-1/2">
+                    <div className="bg-[#262626]">
+                        <Image
+                            src="/bike-together.svg"
+                            alt="two people biking together"
+                            layout="responsive"
+                            width={1600}
+                            height={400}
+                            priority
+                        />
+                    </div>
+                    <div className="pt-4">
+                        <p className="text-4xl font-semibold pb-4">Sustainable manufacturing practices</p>
+                        <p className="text-2xl text-neutral-400">
+                            We have adopted environmentally-friendly manufacturing practices and source all our materials sustainably.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
