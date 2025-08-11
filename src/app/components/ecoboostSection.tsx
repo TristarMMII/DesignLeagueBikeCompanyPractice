cf261c97a90ab31c380712b2a07ca0f653805de0
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-08-10 23:59:20 -0400

    Completed ecoboost section
    added images and implemented fontawesome for icons

--- src/app/components/ecoboostSection.tsx
+++ src/app/components/ecoboostSection.tsx
@@ -1,10 +1,92 @@
-export default function ecoboostSection() {
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+export default function EcoboostSection() {
     return (
-        <div className="bg-white py-30 px-10 flex items-center justify-center flex-col">
-            <div className="text-4xl text-center">
-                Leading the Way in <br /> Sustainable Transportation
+        <div className="relative bg-white py-30 px-10 flex items-center justify-center flex-col overflow-hidden">
+            {/* Background text */}
+            <div className="absolute inset-0 text-[20rem] leading-[0.8] font-normal select-none pointer-events-none">
+                {Array.from({ length: 4 }).map((_, rowIndex) => (
+                    <div
+                        key={rowIndex}
+                        className="whitespace-nowrap"
+                        style={{
+                            transform: `translateX(${(-20 * rowIndex) - 10}%)`
+                        }}
+                    >
+                        {Array.from({ length: 20 }).map((_, i) => (
+                            <span key={i} className="mr-6">
+                                ECOBOOST -
+                            </span>
+                        ))}
+                    </div>
+                ))}
             </div>
 
+            {/* Foreground content */}
+            <div className="relative z-10 flex justify-center items-center w-full">
+
+                {/* Orange box */}
+                <div
+                    className="absolute z-0 bg-[#FE610B] overflow-hidden"
+                    style={{
+                        transform: 'rotate(5deg)',
+                    }}
+                >
+                    <Image
+                        src="/girl-on-bike.svg"
+                        alt=""
+                        width={600}
+                        height={600}
+                        priority
+                        className="invisible"
+                    />
+                </div>
+
+                {/* Background skewed copy */}
+                <div className="absolute z-0" style={{ transform: 'rotate(-3deg)' }}>
+                    <Image
+                        src="/hands-on-bike.svg"
+                        alt=""
+                        width={675}
+                        height={675}
+                        priority
+                    />
+                </div>
+
+                {/* Foreground image */}
+                <div className="relative z-10">
+                    <Image
+                        src="/girl-on-bike.svg"
+                        alt="two people biking together"
+                        width={600}
+                        height={600}
+                        priority
+                    />
+                </div>
+
+                {/* Left arrow button */}
+                <button
+                    aria-label="Previous"
+                    className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-[#FE610B] rounded-full w-25 h-25 flex justify-center items-center shadow-lg hover:bg-orange-600 transition"
+
+                >
+                    <FontAwesomeIcon size="xl" icon={faArrowLeft} />
+
+                </button>
+
+                {/* Right arrow button */}
+                <button
+                    aria-label="Next"
+                    className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-[#FE610B] rounded-full w-25 h-25 flex justify-center items-center shadow-lg hover:bg-orange-600 transition"
+
+                >
+                    <FontAwesomeIcon size="xl" icon={faArrowRight} />
+                </button>
+
+
+            </div>
         </div>
     );
 }
