8626089fd534422b15b9f25a96682e135034d91e
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-08-15 23:38:21 -0400

    updated the info section
    made it so that the text is overlayed ontop of the image

--- src/app/components/informationSection.tsx
+++ src/app/components/informationSection.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 export default function InformationSection() {
     return (
         <div className="bg-black text-white py-30 px-10 flex items-center justify-center flex-col">
-            <div className="text-4xl text-center">
+            <div className="text-5xl text-center">
                 Leading the Way in <br /> Sustainable Transportation
             </div>
 
             {/* E-bike light section */}
-            <div className="bg-[#262626] mt-20 w-[95%]">
+            <div className="bg-[#262626] mt-30 w-[95%] relative">
                 <Image
                     src="/E-bike-light.svg"
                     alt="Ebike front light"
@@ -17,13 +17,16 @@ export default function InformationSection() {
                     height={400}
                     priority
                 />
+
+                {/* Overlayed text */}
+                <div className="absolute bottom-15 left-15 text-left max-w-xl">
+                    <p className="text-4xl font-semibold pb-2">High-quality materials</p>
+                    <p className="text-2xl text-neutral-400">
+                        Premium materials and advanced construction techniques to ensure durability and a long lifespan
+                    </p>
+                </div>
             </div>
-            <div className="w-[95%] pt-4">
-                <p className="text-4xl font-semibold pb-4">High-quality materials</p>
-                <p className="text-2xl text-neutral-400">
-                    Premium materials and advanced construction techniques to ensure durability and a long lifespan
-                </p>
-            </div>
+
 
             {/* Row of 2 image sections side by side */}
             <div className="flex flex-row mt-20 w-[95%] gap-5">
