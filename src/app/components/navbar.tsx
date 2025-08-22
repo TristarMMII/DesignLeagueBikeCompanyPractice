50fe270206b8ea7c1589e3fc49136523a5c12dab
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-08-21 21:29:56 -0400

    mobile stylings

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,26 +1,44 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Navbar() {
-
-
     return (
-        <header className="background p-5 flex items-center justify-between">
-
-            <div className="ml-15 text-white text-4xl flex items-center justify-between">
-                <Image className="mr-3 mt-3" src="/logo.png" alt="Company Logo" width={45} height={45} />
+        <header
+            className="background flex flex-col items-center gap-3 p-3
+                 md:flex-row md:justify-around md:gap-6 md:p-4
+                 lg:justify-between lg:p-5"
+        >
+            {/* Logo */}
+            <div className="flex items-center text-white text-2xl md:text-3xl lg:text-4xl md:ml-5 lg:ml-15">
+                <Image
+                    className="mr-2 md:mr-2 lg:mr-3 lg:mt-3"
+                    src="/logo.png"
+                    alt="Company Logo"
+                    width={40}
+                    height={40}
+                />
                 Recycle
-                <span className="text-2xl align-top">&reg;</span>
+                <span className="text-lg md:text-xl lg:text-2xl align-top">&reg;</span>
             </div>
 
-            <div className="text-white space-x-15 text-lg pr-5">
+            {/* Nav buttons */}
+            <div
+                className="flex w-full justify-center text-white space-x-6 text-sm
+                   md:w-auto md:justify-center md:space-x-8 md:text-base
+                   lg:justify-end lg:space-x-15 lg:text-lg lg:pr-5"
+            >
                 <button>E-bikes</button>
                 <button>Specs</button>
                 <button>Our Impact</button>
             </div>
 
-            <button className="text-white bg-black p-3 px-5 mr-15 rounded-full">Order now</button>
-
-
+            {/* CTA */}
+            <button
+                className="text-white bg-black py-2 px-4 rounded-full
+                   md:py-2 md:px-5 md:rounded-xl
+                   lg:p-3 lg:px-5 lg:mr-15 lg:rounded-full"
+            >
+                Order now
+            </button>
         </header>
     );
 }
