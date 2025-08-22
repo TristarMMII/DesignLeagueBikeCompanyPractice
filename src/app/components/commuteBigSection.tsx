ddcedfdf9cfb6c9cfca68f75b7cb4c98523f3216
Nicholas Cammisuli <nicholas.cammisuli@gmail.com>
2025-08-22 00:40:41 -0400

    mobile friendly

--- src/app/components/commuteBigSection.tsx
+++ src/app/components/commuteBigSection.tsx
@@ -17,9 +17,11 @@ export default function commuteBigSection() {
             </div>
 
             {/* Overlayed text */}
-            <h1 className="absolute bottom-[33%] left-1/2 transform -translate-x-1/2 text-[20rem] text-white z-0 leading-none pointer-events-none select-none">
+            <h1 className="absolute bottom-[33%] left-1/2 transform -translate-x-1/2 text-[4rem] sm:text-[6.5rem] md:text-[9rem] lg:text-[12rem] xl:text-[16rem] 2xl:text-[20rem] text-white z-0 leading-none pointer-events-none select-none">
                 C<span className="italic">O</span>MMUTE
             </h1>
+
+
 
         </div>
     );
